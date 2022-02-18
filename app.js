f242d81521e07e43ea811df9a69941dae080f476
Shant Dashjian <shantgdashjian@gmail.com>
2022-02-18 13:02:23 -0800

    Add pipeline message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Have a Blast!'));
+app.get('/', (req, res) => res.send('Hello World! This is a continuous delivery pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
